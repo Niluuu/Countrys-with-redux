8cbb0a3cf512058ee6d888378a89c41ce59338d9
Nilufar <example@mail.ru>
2019-06-27 19:02:41 +0500

    fetch by redux

--- src/App.js
+++ src/App.js
@@ -1,26 +1,105 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import { connect } from "react-redux";
+import { fetchProducts } from "./productActions";
+import Table from '@material-ui/core/Table';
+import TableBody from '@material-ui/core/TableBody';
+import TableCell from '@material-ui/core/TableCell';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import Paper from '@material-ui/core/Paper';
+import SearchIcon from '@material-ui/icons/Search';
+import InputBase from '@material-ui/core/InputBase';
+import IconButton from '@material-ui/core/IconButton';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+
+export class App extends Component {
+  state = {
+    filtered: [],
+    value: ""
+  }
+  componentDidMount() {
+    this.props.dispatch(fetchProducts());
+  }
+
+  handleChage = (e) => {
+    const { value } = this.state.value
+    this.setState({ value: e.target.value })
+    console.log("val", value)
+  }
+
+   render() {
+    const { error, loading, products } = this.props;
+    // console.log("pr",products)
+
+    if (error) {
+      return <div>Error! {error.message}</div>;
+    }
+
+    if (loading) {
+      return <div>Loading...</div>;
+    }
+
+    return (
+      <div>
+        <Grid
+          container
+          direction="row"
+          justify="center"
+          alignItems="flex-start"
         >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+          <InputBase
+            placeholder="Search Country by name"
+            onChange={this.handleChage}
+            value={this.state.value}
+          />
+          <IconButton aria-label="Search" >
+            <SearchIcon />
+          </IconButton>
+        </Grid>
+        <Grid
+          container
+          direction="row-reverse"
+          justify="center"
+          alignItems="flex-start"
+         >
+        <Paper>
+          <Table>
+            <TableHead>
+              <TableRow>
+                <TableCell>Name of country</TableCell>
+                <TableCell align="right">Capitals</TableCell>
+                <TableCell align="right">Algha code</TableCell>
+                <TableCell align="right">Flags</TableCell>
+              </TableRow>
+            </TableHead>
+            <TableBody>
+              { products.map(country => (
+                  <TableRow key={country.area} >
+                    <TableCell component="th" scope="row">{country.name}</TableCell>
+                    <TableCell align="right">{country.capital}</TableCell>
+                    <TableCell align="right">{country.alpha3Code}</TableCell>
+                    <TableCell style={{backgroundImage: `url(${country.flag})`, backgroundSize: "cover", height: "30px", width: "50px" }} align="right"></TableCell>
+                  </TableRow>
+              ))}
+            </TableBody>
+          </Table>
+        </Paper>
+        </Grid>
+      </div>
+             
+    );
+  }
 }
 
-export default App;
+
+const mapStateToProps = state => ({
+  products: state.products.items,
+  loading: state.products.loading,
+  error: state.products.error
+});
+
+
+export default connect(mapStateToProps) (App)
+
